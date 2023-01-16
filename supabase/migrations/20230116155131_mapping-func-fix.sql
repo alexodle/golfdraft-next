745718f95237920d75b5380fee04325345ffe658
alex.odle <alex.odle@cloudkitchens.com>
2023-01-16 07:52:01 -0800

    fixes

--- supabase/migrations/20230116155131_mapping-func-fix.sql
+++ supabase/migrations/20230116155131_mapping-func-fix.sql
@@ -1,8 +1,10 @@
-CREATE OR REPLACE FUNCTION get_user_mappings()
-RETURNS TABLE ("userId" int, "profileId" uuid, "email" varchar)
-SECURITY DEFINER
-AS
-$$
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_user_mappings()
+ RETURNS TABLE("userId" integer, "profileId" uuid, email character varying)
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
 BEGIN
     IF NOT is_commissioner_of(get_active_tourney_id()) THEN
         RAISE EXCEPTION 'You are not the commissioner of the active tourney';
@@ -14,5 +16,7 @@ BEGIN
     LEFT JOIN gd_user_map ON gd_user_map."profileId" = auth.users.id
     WHERE confirmed_at IS NOT NULL;
 END;
-$$
-LANGUAGE plpgsql;
+$function$
+;
+
+
