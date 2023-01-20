28b109c6bac337e75bb7734fa88efff9ffe44320
alex.odle <alex.odle@cloudkitchens.com>
2023-01-20 10:47:35 -0800

    prefetch for draft

--- pages/draft/[tourneyId].tsx
+++ pages/draft/[tourneyId].tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { QueryClient } from 'react-query';
 import App from '../../lib/App';
 import { AutoPicker } from '../../lib/components/AutoPicker';
+import { prefetchDraftPicks } from '../../lib/data/draft';
+import { prefetchGolfers } from '../../lib/data/golfers';
+import { prefetchTourney } from '../../lib/data/tourney';
+import { prefetchAllUsers } from '../../lib/data/users';
 import AppHeader from '../../lib/legacy/js/components/AppHeader';
 import DraftApp from '../../lib/legacy/js/components/DraftApp';
 import { withAuth } from '../../lib/util/withAuth';
@@ -33,7 +38,21 @@ const Draft: NextPage = () => {
   );
 };
 
-export const getServerSideProps = withAuth(async (props) => {
+export const getServerSideProps = withAuth(async (props, ctx, supabase) => {
+  const queryClient = new QueryClient();
+
+  const tourneyId = +(ctx.query.tourneyId as string);
+  if (isNaN(tourneyId)) {
+    return { redirect: { permanent: false, destination: '/' } };
+  }
+
+  await Promise.all([
+    prefetchTourney(tourneyId, queryClient, supabase),
+    prefetchDraftPicks(tourneyId, queryClient, supabase),
+    prefetchGolfers(tourneyId, queryClient, supabase),
+    prefetchAllUsers(queryClient, supabase),
+  ]);
+
   return { props };
 });
 
