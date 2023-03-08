bd2b72c3151d338e7999b033113787894f2e9115
alex.odle <alex.odle@cloudkitchens.com>
2023-03-08 12:05:27 -0800

    fix css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
-// import '../lib/legacy/less/app.less';
-// import '../lib/legacy/less/app_header.less';
-// import '../lib/legacy/less/bootstrap_repl.less';
-// import '../lib/legacy/less/chat.less';
-// import '../lib/legacy/less/tourney_app.less';
+import '../lib/legacy/less/app.css';
+import '../lib/legacy/less/app_header.css';
+import '../lib/legacy/less/bootstrap_repl.css';
+import '../lib/legacy/less/chat.css';
+import '../lib/legacy/less/tourney_app.css';
 
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
