d9d5a7298ec27eed51ad67d096e51d05e5567b0d
alex.odle <alex.odle@cloudkitchens.com>
2023-05-17 12:03:21 -0700

    include migration

---
+++ supabase/migrations/20230517185943_pick-chat-messages.sql
@@ -0,0 +1,209 @@
+alter table "public"."chat_message" alter column "userId" drop not null;
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.make_pick(tourney_id integer, user_id integer, pick_number integer, golfer_id integer)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+  next_pick_user_id int;
+  next_pick_number int;
+  already_picked_golfer_id int;
+  picked_by_user_id int;
+  pick_proxy_message varchar;
+  tx varchar;
+BEGIN
+  SELECT pg_try_advisory_xact_lock(tourney_id) INTO tx;
+  IF tx <> 'true' THEN
+    RETURN;
+  END IF;
+
+  -- Validate is current pick
+  SELECT "userId", "pickNumber" INTO next_pick_user_id, next_pick_number
+  FROM get_next_pick(tourney_id);
+  IF next_pick_user_id <> user_id THEN
+    raise exception 'Invalid user: %, expected %', user_id, next_pick_user_id;
+  END IF;
+  IF next_pick_number <> pick_number THEN
+    raise exception 'Invalid pick number: %, expected %', pick_number, next_pick_number;
+  END IF;
+
+  SELECT get_user_id(auth.uid()) INTO picked_by_user_id;
+
+  UPDATE draft_pick
+  SET
+    "golferId" = golfer_id,
+    "pickedByUserId" = picked_by_user_id,
+    "timestampEpochMillis" = extract(epoch from now()) * 1000,
+    "clientTimestampEpochMillis" = extract(epoch from now()) * 1000
+  WHERE
+    "tourneyId" = tourney_id
+    AND "pickNumber" = pick_number
+    AND "userId" = user_id
+    AND "golferId" IS NULL;
+
+  pick_proxy_message = '';
+  IF picked_by_user_id != user_id THEN
+    SELECT ' (proxy by ' || "name" || ')' INTO pick_proxy_message
+    FROM gd_user
+    WHERE id = picked_by_user_id;
+  END IF;
+
+  INSERT INTO chat_message ("tourneyId", message)
+  SELECT tourney_id, 'Pick #' || pick_number || ': ' || u.name || ' selects ' || g.name || pick_proxy_message
+  FROM gd_user AS u, golfer AS g
+  WHERE u.id = user_id AND g.id = golfer_id;
+  
+  RETURN;
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.make_pick_list_or_wgr_pick(tourney_id integer, user_id integer, pick_number integer)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+  next_pick_user_id int;
+  next_pick_number int;
+  pick_list_golfer_id int;
+  wgr_golfer_id int;
+  golfer_to_pick_id int;
+  picked_by_user_id int;
+  auto_pick_user_id int;
+  picked_via varchar;
+  tx varchar;
+BEGIN
+  SELECT pg_try_advisory_xact_lock(tourney_id) INTO tx;
+  IF tx <> 'true' THEN
+    RETURN;
+  END IF;
+
+  -- Validate is current pick
+  SELECT "userId", "pickNumber" INTO next_pick_user_id, next_pick_number
+  FROM get_next_pick(tourney_id);
+  IF next_pick_user_id <> user_id THEN
+    raise exception 'Invalid user: %, expected %', user_id, next_pick_user_id;
+  END IF;
+  IF next_pick_number <> pick_number THEN
+    raise exception 'Invalid pick number: %, expected %', pick_number, next_pick_number;
+  END IF;
+
+  SELECT "golferId" INTO pick_list_golfer_id
+  FROM draft_pick_list
+  WHERE "tourneyId" = tourney_id AND "userId" = user_id AND "golferId" NOT IN (
+    SELECT "golferId"
+    FROM draft_pick
+    WHERE "tourneyId" = tourney_id AND "golferId" IS NOT NULL
+  )
+  ORDER BY "pickOrder" ASC
+  LIMIT 1;
+
+  SELECT "id" INTO wgr_golfer_id FROM (
+    SELECT "id", "wgr"
+    FROM golfer
+    WHERE "tourneyId" = tourney_id AND "id" NOT IN (
+      SELECT "golferId"
+      FROM draft_pick
+      WHERE "tourneyId" = tourney_id AND "golferId" IS NOT NULL
+    )
+    ORDER BY wgr ASC
+    LIMIT 7 -- TODO configurable
+  ) as next_golfers
+  ORDER BY wgr DESC
+  LIMIT 1;
+
+  IF pick_list_golfer_id IS NOT NULL THEN
+    golfer_to_pick_id := pick_list_golfer_id;
+    picked_via = 'Pick List';
+  ELSE
+    golfer_to_pick_id := wgr_golfer_id;
+    picked_via = 'WGR+7';
+  END IF;
+  IF golfer_to_pick_id IS NULL THEN
+     raise exception 'No golfer found for auto pick';
+  END IF;
+
+  SELECT "userId" INTO auto_pick_user_id
+  FROM draft_auto_pick
+  WHERE "tourneyId" = tourney_id AND "userId" = user_id;
+
+  IF auto_pick_user_id IS NULL THEN
+    SELECT get_user_id(auth.uid()) INTO picked_by_user_id;
+  END IF;
+
+  UPDATE draft_pick
+  SET
+    "golferId" = golfer_to_pick_id,
+    "pickedByUserId" = picked_by_user_id,
+    "timestampEpochMillis" = extract(epoch from now()) * 1000,
+    "clientTimestampEpochMillis" = extract(epoch from now()) * 1000
+  WHERE
+    "tourneyId" = tourney_id
+    AND "pickNumber" = pick_number
+    AND "userId" = user_id
+    AND "golferId" IS NULL;
+  
+  INSERT INTO chat_message ("tourneyId", message)
+  SELECT tourney_id, 'Pick #' || pick_number || ': ' || u.name || ' selects ' || g.name || ' (' || picked_via || ')'
+  FROM gd_user AS u, golfer AS g
+  WHERE u.id = user_id AND g.id = golfer_to_pick_id;
+  
+  RETURN;
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.undo_last_pick(tourney_id integer)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+    commissioner_user_id int;
+    last_pick_number int;
+    tx varchar;
+BEGIN
+    IF NOT is_commissioner_of(tourney_id) THEN
+        raise exception 'Only the commissioner can undo the last pick';
+    END IF;
+
+    SELECT pg_try_advisory_xact_lock(tourney_id) INTO tx;
+    IF tx <> 'true' THEN
+        RETURN;
+    END IF;
+
+    SELECT "pickNumber" INTO last_pick_number
+    FROM draft_pick
+    WHERE "tourneyId" = tourney_id AND "golferId" IS NOT NULL
+    ORDER BY "pickNumber" DESC
+    LIMIT 1;
+
+    IF last_pick_number IS NULL THEN
+        raise exception 'No picks have been made yet';
+    END IF;
+    
+    UPDATE draft_pick 
+    SET 
+        "golferId" = NULL, 
+        "timestampEpochMillis" = NULL, 
+        "clientTimestampEpochMillis" = NULL, 
+        "pickedByUserId" = NULL
+    WHERE "tourneyId" = tourney_id AND "pickNumber" = last_pick_number;
+
+    INSERT INTO chat_message ("tourneyId", message)
+    VALUES (
+        tourney_id,
+        'Pick #' || last_pick_number || ': rolled back by commissioner'
+    );
+
+    RETURN;
+END;
+$function$
+;
+
+
+
