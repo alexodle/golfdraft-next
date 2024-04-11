b8a92ba2fe903527818f48752577b5f19e9e6854
alex.odle <alex.odle@gmail.com>
2024-04-11 16:02:02 -0400

    turn off update at night

--- pages/api/admin/updateScores.ts
+++ pages/api/admin/updateScores.ts
@@ -5,6 +5,7 @@ import readerConfig from '../../../lib/legacy/scores_sync/readerConfig';
 import * as updateScore from '../../../lib/legacy/scores_sync/updateScore';
 import { TourneyConfig } from '../../../lib/models';
 import { adminSupabase } from '../../../lib/supabase';
+import { toZonedTime } from 'date-fns-tz';
 
 if (!process.env.ADMIN_SCRIPT_API_KEY?.length) {
   throw new Error('Missing ADMIN_SCRIPT_API_KEY env var');
@@ -12,13 +13,20 @@ if (!process.env.ADMIN_SCRIPT_API_KEY?.length) {
 
 async function updateScoresApi(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(404).end();
+    res.status(404).end();
+    return;
   }
   if (req.headers['gd-admin-script-api-key'] !== process.env.ADMIN_SCRIPT_API_KEY) {
-    return res.status(401).end();
+    res.status(401).end();
+    return;
   }
 
-  // res.send('Running');
+  // TODO customizable hours per tournament
+  const now = new Date();
+  if (now.getUTCHours() >= 1 && now.getUTCHours() < 12) {
+    res.status(200).end();
+    return;
+  }
 
   const supabase = adminSupabase();
   const tourneyId = await getActiveTourneyId(supabase);
@@ -35,11 +43,5 @@ async function updateScoresApi(req: NextApiRequest, res: NextApiResponse) {
 
   res.status(201).end();
 }
-
-// export const runtime = 'edge';
-
-// export const config = {
-//   maxDuration: 30, // seconds
-// };
 
 export default updateScoresApi;
