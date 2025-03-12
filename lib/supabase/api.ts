788bbe637fe7153fea91105d578045cf7358f8af
Alex Odle <alex.odle@vannevarlabs.com>
2025-03-11 21:00:43 -0400

    updates

--- lib/supabase/api.ts
+++ lib/supabase/api.ts
@@ -1,21 +1,27 @@
-import { createServerClient, type CookieOptions, serialize } from '@supabase/ssr';
-import { type NextApiRequest, type NextApiResponse } from 'next';
+
+import { createServerClient, serializeCookieHeader } from '@supabase/ssr'
+import { type NextApiRequest, type NextApiResponse } from 'next'
 import { supabaseAnonKey, supabaseUrl } from './constants';
 
 export default function createClient(req: NextApiRequest, res: NextApiResponse) {
-  const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
-    cookies: {
-      get(name: string) {
-        return req.cookies[name];
+  const supabase = createServerClient(
+    supabaseUrl, supabaseAnonKey,
+    {
+      cookies: {
+        getAll() {
+          return Object.keys(req.cookies).map((name) => ({ name, value: req.cookies[name] || '' }))
+        },
+        setAll(cookiesToSet) {
+          res.setHeader(
+            'Set-Cookie',
+            cookiesToSet.map(({ name, value, options }) =>
+              serializeCookieHeader(name, value, options)
+            )
+          )
+        },
       },
-      set(name: string, value: string, options: CookieOptions) {
-        res.appendHeader('Set-Cookie', serialize(name, value, options));
-      },
-      remove(name: string, options: CookieOptions) {
-        res.appendHeader('Set-Cookie', serialize(name, '', options));
-      },
-    },
-  });
+    }
+  )
 
-  return supabase;
+  return supabase
 }
